18b9acddea8f113e5557bd52ca6265b5f09bd20c
AllanBaron94 <allan_b_92@hotmail.com>
2022-11-07 13:25:43 +0100

    add lesson3 changes

--- leccion-3/dataType.js
+++ leccion-3/dataType.js
@@ -1,3 +1,8 @@
 const numero1 = 15;
 const numero2 = 25;
 console.log(numero1+numero2);
+const numero3 = 37;
+const numero4 = 21;
+console.log(numero3-numero2);
+console.log(numero4*numero2);
+console.log(numero3/numero2);
