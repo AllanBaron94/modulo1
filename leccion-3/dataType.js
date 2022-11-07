333537097b8dec347dce036d3bdd7475183bba95
AllanBaron94 <allan_b_92@hotmail.com>
2022-11-07 14:02:23 +0100

    add execises 4,5 and 6

--- leccion-3/dataType.js
+++ leccion-3/dataType.js
@@ -1,3 +1,4 @@
+//ejercicio 3
 const numero1 = 15;
 const numero2 = 25;
 console.log(numero1+numero2);
@@ -6,3 +7,31 @@ const numero4 = 21;
 console.log(numero3-numero2);
 console.log(numero4*numero2);
 console.log(numero3/numero2);
+//ejercicio 4
+const saludo = 'hola';
+if(saludo === 'hola') {
+    console.log('Has saludado');
+}
+//ejercicio 5
+const pokemons = ['squirtle', 'charmander', 'bulbasaur'];
+console.log(`Los primeros 3 pokemones son:${pokemons[0]}, ${pokemons[1]} y ${pokemons[2]}.`);
+//ejercicio 6
+const pokemons1 = [
+    {nombre: 'Pikachu',
+     tipo: 'electrico'
+    },
+    {nombre: 'Gastly',
+     tipo: 'fantasma'      
+    },
+    {nombre:'Charizard',
+     tipo: 'fuego'
+    }
+];
+
+pokemons1.forEach(pokemon => {
+if(pokemon.tipo === 'fuego') {
+    console.log(`El pokemon ${pokemon.nombre} es un pokemon de fuego!`);
+} else {
+    console.log('Tipo de pokemon no identificado!');
+}
+});
