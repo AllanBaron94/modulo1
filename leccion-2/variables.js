9552564c2852efc58d2e0a8c033202e8ebfb5665
AllanBaron94 <allan_b_92@hotmail.com>
2022-11-07 13:04:52 +0100

    add lesson2

---
+++ leccion-2/variables.js
@@ -0,0 +1,15 @@
+// variables
+const nombre = 'allan baron';
+let edad = 28;
+const mayorDeEdad = true;
+let direccion = {
+    calle: 'Mercedesstr',
+    numero: '7/3',
+    codigoPostal:71063,
+    municipio: 'Sindelfingen'
+}
+const coloresFavoritos = ['azul', 'gris', 'verde'];
+const lenguajesFavoritos = ['javascript', 'python', 'flutter'];
+const mejorLenguaje = lenguajesFavoritos[0];
+const peorLenguaje = lenguajesFavoritos[2];
+
